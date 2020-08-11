cb858c0a194a9177d50ffa8615bc200b04901e57
saurabh <saurabhbharti9@gmail.com>
2020-08-11 12:46:20 +0530

    added tweet btn styles

--- src/components/TweetBox.js
+++ src/components/TweetBox.js
@@ -8,9 +8,14 @@ function TweetBox() {
         <div className="tweet--input">
           <Avatar src={process.env.PUBLIC_URL + "/saurabh (2).jpg"}></Avatar>
           <input placeholder="What's happening?" type="text" />
+          <input
+            lassName="tweet--inputImage"
+            placeholder="Enter image URL"
+            type="text"
+          />
         </div>
+        <Button className="tweetBox-button">Tweet</Button>
       </form>
-      <Button>Tweet</Button>
     </div>
   );
 }
