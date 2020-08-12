67c9a51a8d286fca95a0f726a6a6d8d2f025923e
saurabh <saurabhbharti9@gmail.com>
2020-08-12 07:03:42 +0530

    added post fe

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,6 +2,10 @@ import React from "react";
 import "../css/Post.css";
 import { Avatar } from "@material-ui/core";
 import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
+import PublishIcon from "@material-ui/icons/Publish";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import RepeatIcon from "@material-ui/icons/Repeat";
 function Post({
   displayName,
   userName,
@@ -14,21 +18,29 @@ function Post({
   return (
     <div className="post">
       <div className="post__avatar">
-        <Avatar src={process.env.PUBLIC_URL}></Avatar>
+        <Avatar src={process.env.PUBLIC_URL + "/logo512.png"}></Avatar>
       </div>
       <div className="post__body">
         <div className="post__header">
           <div className="post__headerText">
             <h3>
-              UserName
-              <span>
+              UserName{" "}
+              <span className="post__verified">
                 <VerifiedUserIcon className="post__badge" />
+                @username
               </span>
             </h3>
           </div>
           <div className="post_headerDescription">
             <p>Challenge you </p>
           </div>
+        </div>
+        <img src={process.env.PUBLIC_URL + "/EfJKhHAXsAAlIFu.jpeg"} alt="" />
+        <div className="post__footer">
+          <ChatBubbleOutlineIcon fontSize="small" />
+          <RepeatIcon fontSize="small" />
+          <FavoriteBorderIcon fontSize="small" />
+          <PublishIcon fontSize="small" />
         </div>
       </div>
     </div>
