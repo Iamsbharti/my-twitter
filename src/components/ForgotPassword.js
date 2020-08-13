9d2826b5322f7bb98b1e17dd237a493a61f876c1
saurabh <saurabhbharti9@gmail.com>
2020-08-13 07:55:06 +0530

    added component and styles to forgotpassword

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -3,15 +3,17 @@ import { Button } from "@material-ui/core";
 
 function ForgotPassword() {
   return (
-    <div className="forgot__password">
+    <div>
       <div className="forgot__nav">
         <img src={process.env.PUBLIC_URL + "/apple-icon-114x114.png"} alt="" />
-        Password Reset
+        <p>Password Reset</p>
       </div>
-      <h2>Find your Twitter account</h2>
-      <code>Enter your email, phone number, or username.</code>
-      <input type="text" />
-      <Button>Search</Button>
+      <div className="forgot__password">
+        <h2>Find your Twitter account</h2>
+        <code>Enter your email, phone number, or username.</code>
+        <input type="text" />
+        <Button>Search</Button>
+      </div>
     </div>
   );
 }
