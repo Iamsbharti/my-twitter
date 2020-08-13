b581efae187b2d5c6e103992167ca3f341086d23
saurabh <saurabhbharti9@gmail.com>
2020-08-13 06:39:38 +0530

    added home page styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Button } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import "../css/Home.css";
 function Home() {
+  let history = useHistory();
   return (
     <div className="home">
       <img src={process.env.PUBLIC_URL + "/apple-icon-114x114.png"} alt="" />
@@ -12,7 +13,12 @@ function Home() {
         <h3>Join Twitter today.</h3>
       </div>
       <div className="home__buttons">
-        <Button className="button__signup">Sign up</Button>
+        <Button
+          className="button__signup"
+          onClick={(e) => history.push("/signup")}
+        >
+          Sign up
+        </Button>
         <Button className="button__login">Log in</Button>
       </div>
     </div>
