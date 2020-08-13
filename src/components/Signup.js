e0392f8e84b4de0711dad84760fd060a4a600692
saurabh <saurabhbharti9@gmail.com>
2020-08-13 09:41:19 +0530

    added signup component and styles

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { Button } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 function Signup() {
-  return <div>Signup</div>;
+  let history = useHistory();
+  return (
+    <div className="signup">
+      <div className="signup__content">
+        <div className="signup__nav">
+          <img
+            src={process.env.PUBLIC_URL + "/apple-icon-114x114.png"}
+            alt=""
+          />
+        </div>
+        <p>Create your account</p>
+        <input type="text" placeholder="name" />
+        <input type="text" placeholder="Phone or Email" />
+        <Button>Sign up</Button>
+        <br />
+        <h4 onClick={() => history.push("/")}>Cancel?</h4>
+      </div>
+    </div>
+  );
 }
 export default Signup;
