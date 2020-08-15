91a317bcc51653253784387228e9067ab5a601ee
saurabh <saurabhbharti9@gmail.com>
2020-08-15 16:20:03 +0530

    prod-build

--- server/app.js
+++ server/app.js
@@ -36,6 +36,8 @@ initdb();
 
 /**add router */
 app.use(process.env.API_VERSION, router);
+app.use(notfound);
+app.use(handleError);
 
 /**listen to server */
 let port = process.env.PORT || process.env.API_PORT;
@@ -45,10 +47,8 @@ app.listen(port, () => logger.info(`API Server Running at:${port}`));
 //production config
 if (process.env.NODE_ENV === "production") {
   console.log("prod-env");
-  app.use(express.static(path.resolve(__dirname, "../../build")));
+  app.use(express.static(path.resolve(__dirname, "../build")));
   app.get("/*", (req, res) => {
     res.sendFile(path.resolve("index.html"));
   });
 }
-app.use(notfound);
-app.use(handleError);
