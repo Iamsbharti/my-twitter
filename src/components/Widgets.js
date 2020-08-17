9e792b6160476e4a4c499e5eb5f46f20aa92986d
saurabh <saurabhbharti9@gmail.com>
2020-08-17 12:22:13 +0530

    implemented logout feature

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -3,14 +3,21 @@ import "../css/Widgets.css";
 import Searchicon from "@material-ui/icons/Search";
 import { Button } from "@material-ui/core";
 import { TwitterTweetEmbed } from "react-twitter-embed";
+import { useHistory } from "react-router-dom";
 function Widgets() {
+  let history = useHistory();
+  const handleLogout = () => {
+    /**clear localstorage */
+    localStorage.clear();
+    setTimeout(() => history.push("/login"), 1000);
+  };
   return (
     <div className="widgets">
       <div className="widgets__input">
         <Searchicon className="widgets__search" />
         <input placeholder="Searh twitter" type="text" />
       </div>
-      <Button>Logout</Button>
+      <Button onClick={handleLogout}>Logout</Button>
       <div className="widgets__container">
         <h2>What's Happening</h2>
         <TwitterTweetEmbed tweetId={"840430824289906688"} />
