493ac51274992b3cfe7bcd0630b76604f5a31129
saurabh <saurabhbharti9@gmail.com>
2020-08-17 06:41:03 +0530

    combined post reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,7 @@
 import { userReducer } from "./userReducer";
+import { postReducer } from "./postReducer";
 import { combineReducers } from "redux";
 export default combineReducers({
   user: userReducer,
+  posts: postReducer,
 });
