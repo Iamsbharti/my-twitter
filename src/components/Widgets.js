d4de57fb06548f5e83db71240102429dfc0258ce
saurabh <saurabhbharti9@gmail.com>
2020-08-17 12:41:46 +0530

    prod fix

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -9,7 +9,7 @@ function Widgets() {
   const handleLogout = () => {
     /**clear localstorage */
     localStorage.clear();
-    setTimeout(() => history.push("/home"), 1000);
+    setTimeout(() => history.push("/"), 1000);
   };
   return (
     <div className="widgets">
