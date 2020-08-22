76c6d3ad6fc948fd4f95899a3e08a90a430a6130
saurabh <saurabhbharti9@gmail.com>
2020-08-22 08:18:12 +0530

    added bookicons

--- src/components/Post.js
+++ src/components/Post.js
@@ -7,7 +7,9 @@ import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import DeleteIcon from "@material-ui/icons/Delete";
 import RepeatIcon from "@material-ui/icons/Repeat";
+import BookmarkIcon from "@material-ui/icons/Bookmark";
 import { connect } from "react-redux";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
 import {
   updatePostAction,
   deletePostAction,
@@ -79,17 +81,19 @@ function Post({
   const handlePostClick = (username, postid) => {
     history.push(`/${username}/status/${postid}`);
   };
+  /**toggle tweet options */
+  const handleAddBookMark = (e) => {
+    console.log("adding as bookmark");
+  };
   return (
     <>
       <div className="post">
         <div className="post__avatar">
           <Avatar src={process.env.PUBLIC_URL + "/logo512.png"}></Avatar>
         </div>
+
         <div className="post__body">
-          <div
-            className="post__header"
-            onClick={() => handlePostClick(info.userName, info.postId)}
-          >
+          <div className="post__header">
             <div className="post__headerText">
               <h3>
                 {info.displayName}{" "}
@@ -101,8 +105,16 @@ function Post({
                   />
                   @{info.userName}
                 </span>
+                <div>
+                  <span className="headerDropDown">
+                    <BookmarkIcon
+                      fontSize="medium"
+                      onClick={handleAddBookMark}
+                    />
+                  </span>
+                </div>
               </h3>
-              {userId === info.userId && (
+              {status && userId === info.userId && (
                 <div className="headerDeleteIcon">
                   <span>
                     <DeleteIcon
@@ -113,100 +125,100 @@ function Post({
                 </div>
               )}
             </div>
-
+          </div>
+          <div onClick={() => handlePostClick(info.userName, info.postId)}>
             <div className="post_headerDescription">
               <p>{info.description}</p>
             </div>
-          </div>
-
-          {info.image && <img src={info.image} alt="" />}
-          {status && (
-            <div className="post__details">
-              <p className="post_details__date">
-                {dateFormat(info.createdAt, "h:MM TT.mmmm dS,yyyy")}
-                &nbsp;&nbsp;&nbsp;
-                <span>Twitter for Web</span>
-              </p>
-              <p className="post_details__info">
-                {info.retweets} &nbsp;<span>Retweets</span>{" "}
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                {info.likes} &nbsp;<span>Likes</span>
-              </p>
-            </div>
-          )}
-          <div className="post__footer">
-            <div className="icon__items">
-              <ChatBubbleOutlineIcon
-                className="icons"
-                onClick={() => SetToggle(!toggleComment)}
-                fontSize="small"
-              />
-              {info.comments && info.comments.length > 0 && (
-                <p>{info.comments.length}</p>
-              )}
-            </div>
-            <div className="icon__items">
-              <RepeatIcon
-                fontSize="small"
-                className="icons"
-                onClick={() => tweetsUpdate("retweets")}
-                style={{
-                  color:
-                    info.retweetsBy && info.retweetsBy.includes(userId)
-                      ? "Blue"
-                      : "gray",
-                }}
-              />
-              {info.retweets > 0 && <p>{info.retweets}</p>}
-            </div>
-            <div className="icon__items">
-              {info.likedBy && info.likedBy.includes(userId) ? (
-                <FavoriteIcon
-                  className="icons like_icon"
-                  fontSize="small"
-                  onClick={() => tweetsUpdate("dislike")}
-                />
-              ) : (
-                <FavoriteBorderIcon
-                  className="icons like_icon"
-                  fontSize="small"
-                  onClick={() => tweetsUpdate("likes")}
-                />
-              )}
-              {info.likes > 0 && <p>{info.likes}</p>}
-            </div>
-            <div className="icon__items">
-              <PublishIcon
-                className="icons"
-                fontSize="small"
-                onClick={() => tweetsUpdate("shares")}
-              />
-              {info.shares > 0 && <p>{info.shares}</p>}
-            </div>
-          </div>
-
-          <div className="post__comments" hidden={toggleComment}>
-            <input
-              type="text"
-              placeholder="Tweet your reply"
-              onChange={(e) => setComment(e.target.value)}
-            />
-            <Button onClick={handleAddReply}>Reply</Button>
-          </div>
-        </div>
-      </div>
-      {/**render comments for this tweet */}
-      {
-        <div>
-          {status &&
-            info.comments &&
-            info.comments.map((c, index) => (
-              <div key={index} className="post_single_comments">
-                <Comments info={c} userPost={info.userName} />
+            {info.image && <img src={info.image} alt="" />}
+            {status && (
+              <div className="post__details">
+                <p className="post_details__date">
+                  {dateFormat(info.createdAt, "h:MM TT.mmmm dS,yyyy")}
+                  &nbsp;&nbsp;&nbsp;
+                  <span>Twitter for Web</span>
+                </p>
+                <p className="post_details__info">
+                  {info.retweets} &nbsp;<span>Retweets</span>{" "}
+                  &nbsp;&nbsp;&nbsp;&nbsp;
+                  {info.likes} &nbsp;<span>Likes</span>
+                </p>
               </div>
-            ))}
+            )}
+            <div className="post__footer">
+              <div className="icon__items">
+                <ChatBubbleOutlineIcon
+                  className="icons"
+                  onClick={() => SetToggle(!toggleComment)}
+                  fontSize="small"
+                />
+                {info.comments && info.comments.length > 0 && (
+                  <p>{info.comments.length}</p>
+                )}
+              </div>
+              <div className="icon__items">
+                <RepeatIcon
+                  fontSize="small"
+                  className="icons"
+                  onClick={() => tweetsUpdate("retweets")}
+                  style={{
+                    color:
+                      info.retweetsBy && info.retweetsBy.includes(userId)
+                        ? "Blue"
+                        : "gray",
+                  }}
+                />
+                {info.retweets > 0 && <p>{info.retweets}</p>}
+              </div>
+              <div className="icon__items">
+                {info.likedBy && info.likedBy.includes(userId) ? (
+                  <FavoriteIcon
+                    className="icons like_icon"
+                    fontSize="small"
+                    onClick={() => tweetsUpdate("dislike")}
+                  />
+                ) : (
+                  <FavoriteBorderIcon
+                    className="icons like_icon"
+                    fontSize="small"
+                    onClick={() => tweetsUpdate("likes")}
+                  />
+                )}
+                {info.likes > 0 && <p>{info.likes}</p>}
+              </div>
+              <div className="icon__items">
+                <PublishIcon
+                  className="icons"
+                  fontSize="small"
+                  onClick={() => tweetsUpdate("shares")}
+                />
+                {info.shares > 0 && <p>{info.shares}</p>}
+              </div>
+            </div>
+
+            <div className="post__comments" hidden={toggleComment}>
+              <input
+                type="text"
+                placeholder="Tweet your reply"
+                onChange={(e) => setComment(e.target.value)}
+              />
+              <Button onClick={handleAddReply}>Reply</Button>
+            </div>
+          </div>
         </div>
-      }
+        {/**render comments for this tweet */}
+        {
+          <div>
+            {status &&
+              info.comments &&
+              info.comments.map((c, index) => (
+                <div key={index} className="post_single_comments">
+                  <Comments info={c} userPost={info.userName} />
+                </div>
+              ))}
+          </div>
+        }
+      </div>
     </>
   );
 }
