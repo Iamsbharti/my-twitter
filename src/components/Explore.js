c2789e594fae0426ee2f0470de3ce385583b7f39
saurabh <saurabhbharti9@gmail.com>
2020-08-24 15:08:40 +0530

    refactored code to get hashtags and tweets counts

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,9 +1,47 @@
-import React from "react";
-function Explore() {
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getAllPostsAction } from "../redux/actions/postAction";
+
+function Explore({ userId, tweetCountMap, getAllPostsAction }) {
+  useEffect(() => {
+    /**call get allpost for each reload */
+    getAllPostsAction();
+  }, [userId, getAllPostsAction]);
   return (
     <div>
-      <h3>Explore Page coming soon...</h3>
+      <h3>Explore Page coming soon..{tweetCountMap.size}.</h3>
+      <ul>
+        {tweetCountMap &&
+          [...tweetCountMap.keys()].map((key) => (
+            <li key={key}>
+              <span>{key}</span>
+              <span>{tweetCountMap.get(key)}</span>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
-export default Explore;
+const mapStateToProps = ({ user, posts }) => {
+  /**get all hashtags */
+  console.log("posts::", posts);
+  let hashTags = posts.map((post) => post.hashTags).toString();
+  console.log("hashtags,", hashTags);
+  /**count the hashtags and hence its no of uses */
+  let tweetCountMap = new Map();
+  let count = 1;
+  hashTags
+    .split(",")
+    .map((tag) =>
+      tweetCountMap.get(tag)
+        ? tweetCountMap.set(tag, tweetCountMap.get(tag) + 1)
+        : tweetCountMap.set(tag, count)
+    );
+  console.log("tweetcountmap,", tweetCountMap);
+  [...tweetCountMap.keys()].map((key) => console.log(key));
+  return { userId: user.userId, tweetCountMap };
+};
+const mapActionToProps = {
+  getAllPostsAction,
+};
+export default connect(mapStateToProps, mapActionToProps)(Explore);
