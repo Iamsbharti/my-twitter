134222be8030a849ea5ff6974f305812247dbd76
saurabh <saurabhbharti9@gmail.com>
2020-08-25 13:12:25 +0530

    created profile component and styles

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,26 +1,60 @@
 import React from "react";
-import "../css/Utility.css";
-import { Button } from "@material-ui/core";
+import "../css/Profile.css";
+import { Button, Avatar } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
-function Profile() {
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import SideBar from "./SideBar";
+import Widgets from "./Widgets";
+import { connect } from "react-redux";
+function Profile({ name, email, userId, username }) {
   let history = useHistory();
-  const handleClick = () => {
+  /*const handleClick = () => {
     history.push("/tweets");
+  };*/
+  const handleBackToprofile = () => {
+    history.goBack();
   };
   return (
-    <div className="header">
-      <h3>Feature Not Yet Ready...</h3>
-      <div className="banner">
-        <img
-          src="https://media.giphy.com/media/14ko1wkUS2thu/giphy.gif"
-          alt=""
-        />
+    <>
+      <div className="app">
+        <SideBar />
+        <div className="profile">
+          {localStorage.getItem("authToken") ? (
+            <div className="profileHeader">
+              <div className="status__header">
+                <ArrowBackIcon
+                  fontSize="large"
+                  className="status__icon"
+                  onClick={handleBackToprofile}
+                />
+                <h3>{name}</h3>
+              </div>
+              <div className="profile__coverpic">
+                <img
+                  src={process.env.PUBLIC_URL + "/coverpic.jpeg"}
+                  className="coverPic"
+                  alt=""
+                />
+                <img
+                  className="profile__user__avatar"
+                  src={process.env.PUBLIC_URL + "/saurabh (2).jpg"}
+                  alt=""
+                />
+                <Button>Edit Profile</Button>
+              </div>
+            </div>
+          ) : (
+            history.push("/login")
+          )}
+        </div>
+        <Widgets />
       </div>
-      <div className="bottom">
-        <h3>Stay Tuned!!!!</h3>
-        <Button onClick={handleClick}>Back To Tweets!!!</Button>
-      </div>
-    </div>
+    </>
   );
 }
-export default Profile;
+const mapStateToProps = ({ user }) => {
+  const { name, email, userId, username } = user.user;
+  return { name, email, userId, username };
+};
+const mapActionToProps = {};
+export default connect(mapStateToProps, mapActionToProps)(Profile);
