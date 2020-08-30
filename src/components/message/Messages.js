13267c4bc0ecacc542a0b998265f0b5e34ea8409
saurabh <saurabhbharti9@gmail.com>
2020-08-30 11:13:48 +0530

    added conditional rendering chat users

--- src/components/message/Messages.js
+++ src/components/message/Messages.js
@@ -1,15 +1,9 @@
 import React from "react";
 import "../../css/Message.css";
-import { Button } from "@material-ui/core";
-import { useHistory } from "react-router-dom";
+
 import Sidebar from "../SideBar";
-import ChatBox from "./ChatBox";
 import PeopleList from "./PeopleList";
 function Messages() {
-  let history = useHistory();
-  const handleClick = () => {
-    history.push("/tweets");
-  };
   return (
     <div className="app">
       <Sidebar />
