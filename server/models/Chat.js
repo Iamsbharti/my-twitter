3c6fe1f8461459b4dc6d3c29770fb245bd9cf500
saurabh <saurabhbharti9@gmail.com>
2020-09-02 11:10:51 +0530

    added chat module schema

---
+++ server/models/Chat.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const chatSchema = mongoose.Schema({
+  chatId: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  senderId: {
+    type: String,
+    required: true,
+  },
+  senderName: {
+    type: String,
+    required: true,
+  },
+  recieverId: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  modifiedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  seen: {
+    type: Boolean,
+    default: false,
+  },
+});
+module.exports = mongoose.model("Chat", chatSchema);
