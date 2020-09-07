c4489b756d6a563ae3678116d8a84bbf9149382d
saurabh <saurabhbharti9@gmail.com>
2020-09-08 01:48:06 +0530

    added sidebar responsive

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -8,29 +8,58 @@ import ListAlticon from "@material-ui/icons/ListAlt";
 import PermIdentityicon from "@material-ui/icons/PermIdentity";
 import MoreHorizicon from "@material-ui/icons/MoreHoriz";
 import SideBarOptions from "./SideBarOptions";
-import { Button } from "@material-ui/core";
 import "../css/SideBar.css";
-
-function SideBar() {
+import { Avatar, Button } from "@material-ui/core";
+import { baseUrl } from "../apis/apiUtils";
+import { connect } from "react-redux";
+function SideBar({ userInfo }) {
   return (
-    <div className="sidebar">
-      <img
-        className="icon--img"
-        src={process.env.PUBLIC_URL + "/android-icon-96x96.png"}
-        alt=""
-      />
-      <SideBarOptions text="Home" Icon={Homeicon} active />
-      <SideBarOptions text="Explore" Icon={Searchicon} />
-      <SideBarOptions text="Notifications" Icon={NotificationsNoneicon} />
-      <SideBarOptions text="Messages" Icon={Emailicon} />
-      <SideBarOptions text="Booksmarks" Icon={BookmarkBordericon} />
-      <SideBarOptions text="Lists" Icon={ListAlticon} />
-      <SideBarOptions text="Profile" Icon={PermIdentityicon} />
-      <SideBarOptions text="More" Icon={MoreHorizicon} />
-      <Button className="sidebar__tweet" variant="outlined" fullWidth>
-        Tweet
-      </Button>
-    </div>
+    <>
+      <div className="sidebar">
+        <img
+          className="icon--img"
+          src={process.env.PUBLIC_URL + "/android-icon-96x96.png"}
+          alt=""
+        />
+        <SideBarOptions text="Home" Icon={Homeicon} active />
+        <SideBarOptions text="Explore" Icon={Searchicon} />
+        <SideBarOptions text="Notifications" Icon={NotificationsNoneicon} />
+        <SideBarOptions text="Messages" Icon={Emailicon} />
+        <SideBarOptions text="Booksmarks" Icon={BookmarkBordericon} />
+        <SideBarOptions text="Lists" Icon={ListAlticon} />
+        <SideBarOptions text="Profile" Icon={PermIdentityicon} />
+        <SideBarOptions text="More" Icon={MoreHorizicon} />
+        <Button className="sidebar__tweet" variant="outlined" fullWidth>
+          Tweet
+        </Button>
+      </div>
+      <div className="sidebar__responsive">
+        <div className="sidebar_responsive_avatar">
+          <Avatar
+            src={
+              userInfo &&
+              `${baseUrl}/api/v1/user/fetchPicture?filename=${
+                userInfo.profile.filename
+              }&authToken=${localStorage.getItem("authToken")}`
+            }
+          ></Avatar>
+        </div>
+        <SideBarOptions text="Home" Icon={Homeicon} active />
+        <SideBarOptions text="Explore" Icon={Searchicon} />
+        <SideBarOptions text="Notifications" Icon={NotificationsNoneicon} />
+        <SideBarOptions text="Messages" Icon={Emailicon} />
+        <SideBarOptions text="Booksmarks" Icon={BookmarkBordericon} />
+        <SideBarOptions text="Lists" Icon={ListAlticon} />
+        <SideBarOptions text="Profile" Icon={PermIdentityicon} />
+        <SideBarOptions text="More" Icon={MoreHorizicon} />
+        <Button className="sidebar__tweet" variant="outlined" fullWidth>
+          Tweet
+        </Button>
+      </div>
+    </>
   );
 }
-export default SideBar;
+const mapStateToProps = ({ user }) => {
+  return { userInfo: user.user };
+};
+export default connect(mapStateToProps)(SideBar);
