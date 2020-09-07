5187c8050cd9e568403bcf28da54233652d84f36
saurabh <saurabhbharti9@gmail.com>
2020-09-08 05:01:56 +0530

    responsiveness header fix

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -9,10 +9,8 @@ import PermIdentityicon from "@material-ui/icons/PermIdentity";
 import MoreHorizicon from "@material-ui/icons/MoreHoriz";
 import SideBarOptions from "./SideBarOptions";
 import "../css/SideBar.css";
-import { Button, Avatar } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import { connect } from "react-redux";
-import CloseIcon from "@material-ui/icons/Close";
-import { baseUrl } from "../apis/apiUtils";
 function SideBar({ userInfo }) {
   return (
     <>
@@ -34,57 +32,6 @@ function SideBar({ userInfo }) {
           Tweet
         </Button>
       </div>
-      {/*<div>
-        <div className="sidebar__responsive">
-          <div className="sidebar_responsive_header">
-            <p>Account info</p>
-            <span>
-              <CloseIcon />
-            </span>
-          </div>
-          <div className="sidebar_responsive_avatar">
-            <Avatar
-              src={
-                userInfo &&
-                `${baseUrl}/api/v1/user/fetchPicture?filename=${
-                  userInfo.profile.filename
-                }&authToken=${localStorage.getItem("authToken")}`
-              }
-            ></Avatar>
-          </div>
-          <div className="sidebar_responsive__userinfo">
-            <p>{userInfo.name}</p>
-            <p>@{userInfo.username}</p>
-          </div>
-          <div className="sidebar__responsive__profile">
-            <p>
-              <span className="sidebar__responsive__number">
-                {userInfo.following}
-              </span>
-              <span className="sidebar__responsive__span">Following</span>
-            </p>
-
-            <p>
-              <span className="sidebar__responsive__number">
-                {userInfo.followers && userInfo.followers.length}
-              </span>
-              <span className="sidebar__responsive__span">Followers</span>
-            </p>
-          </div>
-
-          <SideBarOptions text="Home" Icon={Homeicon} active />
-          <SideBarOptions text="Explore" Icon={Searchicon} />
-          <SideBarOptions text="Notifications" Icon={NotificationsNoneicon} />
-          <SideBarOptions text="Messages" Icon={Emailicon} />
-          <SideBarOptions text="Booksmarks" Icon={BookmarkBordericon} />
-          <SideBarOptions text="Lists" Icon={ListAlticon} />
-          <SideBarOptions text="Profile" Icon={PermIdentityicon} />
-          <SideBarOptions text="More" Icon={MoreHorizicon} />
-          <Button className="sidebar__tweet" variant="outlined" fullWidth>
-            Logout
-          </Button>
-        </div>
-      </div>*/}
     </>
   );
 }
