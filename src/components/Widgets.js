bcdc1b03f3667719c6c618be6e3c2825d017b968
saurabh <saurabhbharti9@gmail.com>
2020-09-11 03:08:22 +0530

    implmented dynamic widgets :rocket:

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../css/Widgets.css";
 import Searchicon from "@material-ui/icons/Search";
 import { TwitterTweetEmbed } from "react-twitter-embed";
-function Widgets() {
+import { connect } from "react-redux";
+function Widgets({ user, post, profile }) {
+  const tweetIds = [
+    "1304113292676390914",
+    "840430824289906688",
+    "1294176141440319488",
+    "1294785948325208064",
+    "1304113679521255425",
+    "1304061623854755841",
+    "1304055197149229056",
+    "1302148980365647872",
+    "1303994264574074881",
+    "1302789190699155468",
+  ];
+  const [displayTweets, setDispTweets] = useState([]);
+  const [randomSelection, setRandom] = useState(
+    Math.floor(Math.random() * 4) + 1
+  );
+  useEffect(() => {
+    setDispTweets(tweetIds.splice(randomSelection + 1, randomSelection));
+
+    return () => {
+      setRandom(Math.floor(Math.random() * 4) + 1);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user, post, profile, randomSelection]);
   return (
     <div className="widgets">
       <div className="widgets__input">
@@ -11,16 +36,14 @@ function Widgets() {
       </div>
       <div className="widgets__container">
         <h2>What's Happening</h2>
-        <TwitterTweetEmbed tweetId={"840430824289906688"} />
-        <TwitterTweetEmbed tweetId={"1294176141440319488"} />
-        <TwitterTweetEmbed tweetId={"834159901324754944"} />
-        <div className="centerContent">
-          <div className="selfCenter standardWidth">
-            <TwitterTweetEmbed tweetId="1294785948325208064" />
-          </div>
-        </div>
+        {displayTweets.map((id, index) => (
+          <TwitterTweetEmbed key={index} tweetId={id} />
+        ))}
       </div>
     </div>
   );
 }
-export default Widgets;
+const mapStateToProps = ({ user, post, profile }) => {
+  return { user, post, profile };
+};
+export default connect(mapStateToProps)(Widgets);
