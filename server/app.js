8ab8911ac5d98b409d4d2e01da502d91bb2d75da
saurabh <saurabhbharti9@gmail.com>
2020-09-11 06:03:50 +0530

    refactored folder stucture

--- server/app.js
+++ server/app.js
@@ -50,6 +50,9 @@ app.use(process.env.API_VERSION, router);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 //app.use(notfound);
